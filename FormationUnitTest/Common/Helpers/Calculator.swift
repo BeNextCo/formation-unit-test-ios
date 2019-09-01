d669b7b0d832381698ac7748a3fd0f8a7e20bfe4
Ilan Zerath <ilan.prims@gmail.com>
2019-09-01 12:58:07 +0200

    Add Calculator class

---
+++ FormationUnitTest/Common/Helpers/Calculator.swift
@@ -0,0 +1,16 @@
+//
+//  Calculator.swift
+//  FormationUnitTest
+//
+//  Created by Ilan Z on 01/09/2019.
+//  Copyright © 2019 Ilan Z. All rights reserved.
+//
+
+import Foundation
+
+final class Calculator {
+
+    func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
