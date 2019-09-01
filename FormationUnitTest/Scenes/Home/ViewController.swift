5290e2874179c754a137f5ca2e94608bbd1d62ac
Ilan Zerath <ilan.prims@gmail.com>
2019-09-01 12:53:55 +0200

    Init iOS project

---
+++ FormationUnitTest/Scenes/Home/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FormationUnitTest
+//
+//  Created by Ilan Z on 01/09/2019.
+//  Copyright © 2019 Ilan Z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
