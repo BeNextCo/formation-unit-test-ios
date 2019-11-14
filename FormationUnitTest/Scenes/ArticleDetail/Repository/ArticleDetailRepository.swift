fe98ded2a2d9c5d700771f0a16f9207c4f92c12e
Ilan Zerath <ilan.prims@gmail.com>
2019-11-14 19:57:10 +0100

    08 - TDD

---
+++ FormationUnitTest/Scenes/ArticleDetail/Repository/ArticleDetailRepository.swift
@@ -0,0 +1,39 @@
+//
+//  ArticleDetailRepository.swift
+//  FormationUnitTest
+//
+//  Created by Ilan Z on 14/11/2019.
+//  Copyright © 2019 Ilan Z. All rights reserved.
+//
+
+import Foundation
+
+protocol ArticleProtocol {
+    var id: String? { get }
+    var title: String? { get }
+}
+
+enum ArticleDetailRepositoryError: Error {
+    case erreur1
+    case erreur2
+    case networkErreur
+    case unknown
+}
+
+protocol ArticleDetailRepositoryProtocol {
+    typealias retrieveArticleSuccess = (ArticleProtocol) -> Void
+    typealias retrieveArticleError = (ArticleDetailRepositoryError) -> Void
+
+    func retrieveArticle(success: @escaping retrieveArticleSuccess, failure: @escaping retrieveArticleError)
+}
+
+class ArticleDetailRepository {
+
+}
+
+// MARK: - ArticleDetailRepositoryProtocol
+extension ArticleDetailRepository: ArticleDetailRepositoryProtocol {
+    func retrieveArticle(success: @escaping retrieveArticleSuccess, failure: @escaping retrieveArticleError) {
+        
+    }
+}
